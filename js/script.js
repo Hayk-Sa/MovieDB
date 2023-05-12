0afb554fb5a8032bf925ef9b7ae51d812724d255
Hayk <hayk.sahakyan@codics.am>
2023-05-12 12:03:05 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,81 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+	const movieDB = {
+		movies: [
+			"The Shawshank Redemption",
+			"The Godfather",
+			"The Dark Knight",
+			"12 Angry Men",
+			"Schindler's List",
+		],
+	};
+
+	const adv = document.querySelectorAll(".promo__adv img"),
+		poster = document.querySelector(".promo__bg"),
+		genre = poster.querySelector(".promo__genre"),
+		movieList = document.querySelector(".promo__interactive-list"),
+		addForm = document.querySelector("form.add"),
+		addInput = addForm.querySelector(".adding__input"),
+		checkbox = addForm.querySelector('[type="checkbox"]');
+
+	addForm.addEventListener("submit", (event) => {
+		event.preventDefault();
+
+		let newFilm = addInput.value;
+		const favorite = checkbox.checked;
+
+		if (newFilm) {
+			if (newFilm.length > 21) {
+				newFilm = `${newFilm.substring(0, 22)}...`;
+			}
+			if (favorite) {
+				console.log("Аdd your favorite movie");
+			}
+			movieDB.movies.push(newFilm);
+			sortArr(movieDB.movies);
+			createMovieList(movieDB.movies, movieList);
+		}
+
+		event.target.reset();
+	});
+
+	const deleteAdv = (arr) => {
+		arr.forEach((item) => {
+			item.remove();
+		});
+	};
+
+	const makeChanges = () => {
+		genre.textContent = "drama";
+
+		poster.style.backgroundImage = 'url("img/bg.jpg")';
+	};
+
+	const sortArr = (arr) => {
+		arr.sort();
+	};
+
+	function createMovieList(films, parent) {
+		parent.innerHTML = "";
+		sortArr(films);
+		films.forEach((film, i) => {
+			parent.innerHTML += `
+            <li class="promo__interactive-item">${i + 1} ${film}
+                <div class="delete"></div>
+            </li>`;
+		});
+
+		document.querySelectorAll(".delete").forEach((btn, i) => {
+			btn.addEventListener("click", () => {
+				btn.parentElement.remove();
+				movieDB.movies.splice(i, 1);
+				createMovieList(films, parent);
+			});
+		});
+	}
+	makeChanges();
+
+	deleteAdv(adv);
+	createMovieList(movieDB.movies, movieList);
+});
